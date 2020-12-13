4e7449214310a875cf8862e2d80899e63f3b9271
zhrk <zshchipets@gmail.com>
2020-12-14 00:51:12 +0300

    Add modal logic

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,33 @@
 import Modal from '../Modal';
 import styles from './styles.module.scss';
 
-const App: React.FC = () => (
-  <div className={styles.container}>
-    <Modal />
-  </div>
-);
+const App: React.FC = () => {
+  const content2 = <div>modal2</div>;
+
+  const content = (
+    <div>
+      modal
+      <Modal content={content2}>
+        {({ openModal }) => (
+          <button type="button" onClick={openModal}>
+            second modal
+          </button>
+        )}
+      </Modal>
+    </div>
+  );
+
+  return (
+    <div className={styles.container}>
+      <Modal content={content}>
+        {({ openModal }) => (
+          <button type="button" onClick={openModal}>
+            modal
+          </button>
+        )}
+      </Modal>
+    </div>
+  );
+};
 
 export default App;
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,36 @@
-const Modal: React.FC = () => <div>modal</div>;
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import { useState } from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles.module.scss';
+import ModalContent from '../ModalContent';
+
+interface ModalProps {
+  content: JSX.Element;
+  children: ({ openModal }: { openModal: () => void }) => JSX.Element;
+}
+
+const Modal: React.FC<ModalProps> = (props) => {
+  const { content, children } = props;
+
+  const [isOpen, setOpen] = useState(false);
+
+  const openModal = () => setOpen(true);
+  const closeModal = () => setOpen(false);
+
+  return (
+    <>
+      {isOpen &&
+        ReactDOM.createPortal(
+          <div className={styles.container} onClick={closeModal}>
+            <ModalContent closeModal={closeModal}>{content}</ModalContent>
+          </div>,
+          document.body
+        )}
+      {children({ openModal })}
+    </>
+  );
+};
 
 export default Modal;
